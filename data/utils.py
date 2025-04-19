fd2487be072d263562c2e34fb17b4440e441b783
Song-Daeun <sde11280@hanyang.ac.kr>
2025-04-20 01:30:44 +0900

    feat: wav -> mp3 변환 스크립트 추가

--- data/utils.py
+++ data/utils.py
@@ -1,8 +1,15 @@
 import json
 from typing import List, Dict, Any
+from pydub import AudioSegment
 
 
 def save_json(data: List[Dict[str, Any]], output_path: str = "label_data.json") -> None:
     with open(output_path, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
     print(f"✅ JSON 파일 저장 완료: {output_path}")
+
+
+def convert_wav_to_mp3(input_path, output_path, ffmpeg_path):
+    AudioSegment.converter = ffmpeg_path
+    sound = AudioSegment.from_wav(input_path)
+    sound.export(output_path, format="mp3", bitrate="320k")
